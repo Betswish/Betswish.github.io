2e4ccfed52b2e8e3804a99815896129ab107282e
Jirui Qi <131008659+Betswish@users.noreply.github.com>
2025-03-17 13:47:25 +0100

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -26,19 +26,20 @@ with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
 import requests
 from bs4 import BeautifulSoup
 
-# Old version with Semantic Scholar API
-# from semanticscholar import SemanticScholar
-# sch = SemanticScholar()
-# author_semantic = sch.get_author(2112611646)
-# citation_num = author_semantic.citationCount
+# Version 1: with Semantic Scholar API
+from semanticscholar import SemanticScholar
+sch = SemanticScholar()
+author_semantic = sch.get_author(2112611646)
+citation_num = author_semantic.citationCount
 
-url="https://www.semanticscholar.org/author/Jirui-Qi/2112611646"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
-html=requests.get(url,headers=headers)
-val = BeautifulSoup(html.text, 'html.parser')
+# Version 2: with HTML request
+# url="https://www.semanticscholar.org/author/Jirui-Qi/2112611646"
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
+# html=requests.get(url,headers=headers)
+# val = BeautifulSoup(html.text, 'html.parser')
+# citation_num = val.find_all("span", class_="author-detail-card__stats-row__value")[-2].text
 
-citation_num = val.find_all("span", class_="author-detail-card__stats-row__value")[-2].text
-
+# Format
 shieldio_data_semantic = {
   "schemaVersion": 1,
   "label": "citations_semantic",
